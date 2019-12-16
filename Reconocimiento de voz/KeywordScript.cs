83b97c4914a921c174057fa65870b8501c55434a
alu0100971385 <43812725+alu0100971385@users.noreply.github.com>
2019-12-16 19:36:23 +0000

    Práctica de reconocimiento de voz

---
+++ Reconocimiento de voz/KeywordScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+
+public class KeywordScript : MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+
+    public void Start()
+    {
+        Debug.Log("Start");
+        m_Keywords = new string[] {"hola","coche","hello"};
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log(args.text);
+    }
+}
